2483c32d07eafa3c08bee3803ea0086054f52e34
Darnell Lewis <darlewis@my356.bellevue.edu>
2023-11-29 22:09:20 -0500

    module 8 assignment

---
+++ module_8/movies_update_and_delete.py
@@ -0,0 +1,58 @@
+# Import mysql
+import mysql.connector as sql
+
+# Method show_films
+
+
+def show_films(cursor, title):
+    # method to execute an inner join on all tables,
+    # iterate over the dataset and output the results to the terminal window.
+
+    # inner join query
+    cursor.execute('''select film_name as Name, film_director as Director, genre_name as Genre, studio_name as 'Studio Name' from film INNER JOIN genre ON film.genre_id=genre.genre_id INNER JOIN studio ON film.studio_id=studio.studio_id''')
+    # get the results from the cursor object
+    films = cursor.fetchall()
+    print("\n -- {} --".format(title))
+
+    # iterate over the film data set and display the results
+    for film in films:
+        print("Film Name: {}\nDirector: {}\nGenre Name ID: {}\nStudio Name: {}\n".format(
+            film[0], film[1], film[2], film[3]))
+
+
+# Main method
+def main():
+
+    # Create connection to database
+    connection = sql.connect(
+        host="127.0.0.1",
+        user="root",
+        password="@Fatdaddy22",
+        database="movie"
+    )
+
+    # Create cursor
+    cursor = connection.cursor()
+
+    # Displaying existing records in database
+    show_films(cursor, "DISPLAYING FILMS")
+
+    # Perform Insertion of new movie
+    insertQuery = " INSERT INTO film(film_name, film_releaseDate, film_runtime, film_director, studio_id, genre_id) VALUES ('Man on Fire', '2004', '246', 'Tony Scott', (SELECT studio_id FROM studio WHERE studio_name = '20th Century Fox'),(SELECT genre_id FROM genre WHERE genre_name = 'SciFi') );"
+    connection.commit()
+    cursor.execute(insertQuery)
+    show_films(cursor, "DISPLAYING FILMS AFTER INSERT")
+
+    # Perform Update of Alien movie
+    updateQuery = " update film set genre_id = (select genre_id from genre where genre_name = 'Horror') where film_name = 'Alien';"
+    connection.commit()
+    cursor.execute(updateQuery)
+    show_films(cursor, "DISPLAYING FILMS AFTER UPDATE- Changed Alien to Horror")
+
+    # Delete Gladiator movie from table
+    deleteQuery = " delete from film where film_name = 'Gladiator';"
+    connection.commit()
+    cursor.execute(deleteQuery)
+    show_films(cursor, "DISPLAYING FILMS AFTER Delete")
+
+main()
